885642a8e015115c06031625f023ffeaa12dbf36
darshankardile <darshan.kardil@gmail.com>
2025-12-17 17:39:46 +0530

    many changes frontend

--- src/app/lib/axios.js
+++ src/app/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-note-backend-100.onrender.com",
+  baseURL: "https://mern-note-1-0.onrender.com/api",
 });
 
 export default api;
+
+//"http://localhost:3000/api"
+
+
